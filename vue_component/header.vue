07951e18ca813a845502c6c21f68b11366fe91fd
daidq <daidoquang@vccorp.vn>
2020-04-19 16:24:46 +0700

    update header

--- vue_component/header.vue
+++ vue_component/header.vue
@@ -1,5 +1,87 @@
 <template>
-    <div>
-        
-    </div>
+    <header class="header container">
+        <div class="top-bar">
+            <ul class="link-top">
+                <li>
+                    <a href="#">
+                        Trang chủ   
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Giới thiệu    
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Nhà đầu tư    
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Mạng lưới    
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Tuyển dụng
+                    </a>
+                </li>
+            </ul>
+            <div class="right">
+                <form class="form-search">
+                    <input type="text" placeholder="Tìm kiếm">
+                    <button class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></button>
+                </form>
+                <a href="#" class="language">
+                    <img src="images/ic_en" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="header-main">
+            <div class="logo">
+                <a href="/">
+                    <img src="images/logo.png" alt="">
+                </a>
+            </div>
+            <div class="menu-wrap">
+                <ul class="main-menu">
+                    <li class="menu-item">
+                        <a href="#" class="menu-link">
+                            <span class="icon">
+                                <img src="images/ic_canhan.png" alt="">
+                            </span>
+                            KHÁCH HÀNG CÁ NHÂN
+                            <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" class="menu-link">
+                            <span class="icon">
+                                <img src="images/ic_kh-to-chuc.png" alt="">
+                            </span>
+                            KHÁCH HÀNG TỔ CHỨC
+                            <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" class="menu-link">
+                            <span class="icon">
+                                <img src="images/ic_kh-uu-tien.png" alt="">
+                            </span>
+                            KHÁCH HÀNG ƯU TIÊN
+                            <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <a href="tel:1900545413" class="hot-line">
+                <span class="icon">
+                    <img src="images/ic_hotline.png" alt="">
+                </span>
+                <span class="text-1">Dịch vụ khách hàng 24/7</span>
+                <b class="text-2">1900 54 54 13</b>
+            </a>
+        </div>
+    </header>
 </template>
