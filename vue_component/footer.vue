b5512a6fbbceb80b242ea7d77ab27ca7ab09171a
daidq <daidoquang@vccorp.vn>
2020-04-21 01:27:31 +0700

    footer

--- vue_component/footer.vue
+++ vue_component/footer.vue
@@ -1,5 +1,391 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+        <div class="ft-control">
+            <div class="container d-flex">
+                <a href="javascript:;" class="expand-footer">
+                    Chỉ mục trang
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                </a>
+                <a href="javascript:;" class="go-to-top">
+                    Về đầu trang
+                    <i class="fa fa-angle-up" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+        <div class="box-link-icon">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-7 col-xl-9">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <div class="footer-link">
+                                    <h5 class="footer-link-title">
+                                        LIÊN HỆ
+                                    </h5>
+                                    <ul class="list-link">
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_phone"></i>
+                                                1900 54 54 13
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link" style="white-space: nowrap;">
+                                                <i class="icon ic_email"></i>
+                                                lienhe@vietcombank.com.vn
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <div class="footer-link">
+                                    <h5 class="footer-link-title">
+                                        TÌM VIETCOMBANK
+                                    </h5>
+                                    <ul class="list-link">
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_atm"></i>
+                                                ATM gần nhất
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_vcb"></i>
+                                                Chi nhánh gần nhất
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_location"></i>
+                                                Điểm mua bán ngoại tệ
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <div class="footer-link">
+                                    <h5 class="footer-link-title">
+                                        THÔNG TIN NGÂN HÀNG
+                                    </h5>
+                                    <ul class="list-link">
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_ty-gia"></i>
+                                                Tỷ giá
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_lai-xuat"></i>
+                                                Lãi suất
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_bieu-phi"></i>
+                                                Biểu phí
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_bieu-mau"></i>
+                                                Biểu mẫu
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <div class="footer-link">
+                                    <h5 class="footer-link-title">
+                                        CÔNG CỤ TÍNH TOÁN
+                                    </h5>
+                                    <ul class="list-link">
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_chuyen-doi-ngoai-te"></i>
+                                                Chuyển đổi ngoại tệ
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_lich"></i>
+                                                Tính lịch trả nợ
+                                            </a>
+                                        </li>
+                                        <li class="link-item">
+                                            <a href="" class="link">
+                                                <i class="icon ic_tinh-lai"></i>
+                                                Tính lãi tiết kiệm
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 col-xl-3">
+                        <div class="support-chat">
+                            <a href="#">
+                                <i class="icon ic_chat"></i>
+                                WEBSITE FEEDBACK
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container box-list-link">
+            <div class="row">
+                <div class="col-12 col-xl-9">
+                    <div class="row">
+                        <div class="col-12 col-md-4 col-xl-3">
+                            <div class="footer-link">
+                                <h5 class="footer-link-title">
+                                    KHÁCH HÀNG CÁ NHÂN
+                                </h5>
+                                <ul class="list-link">
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Chương trình khuyến mãi
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Thẻ
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Tài khoản và tiết kiệm
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Cho vay cá nhân
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Chuyển và nhận tiền
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Ngân hàng điện tử
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Bancassurance
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Đầu tư
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-xl-3">
+                            <div class="footer-link">
+                                <h5 class="footer-link-title">
+                                    KHÁCH HÀNG TỔ CHỨC
+                                </h5>
+                                <ul class="list-link">
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Định chế tài chính
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Khách hàng doanh nghiệp
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                           Doanh nghiệp SMEs
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Dịch vụ thẻ
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-xl-3">
+                            <div class="footer-link">
+                                <h5 class="footer-link-title">
+                                   GIỚI THIỆU
+                                </h5>
+                                <ul class="list-link">
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Lịch sử phát triển
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Cơ cấu bộ máy quản lý
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Sứ mệnh và tầm nhìn
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Mục tiêu chiến lược
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Giá trị cốt lõi của thương hiệu
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Bản sắc văn hóa
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Thành tựu và giải thưởng
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-xl-3 d-none d-xl-block">
+                            <div class="footer-link">
+                                <h5 class="footer-link-title">
+                                    NHÀ ĐẦU TƯ
+                                </h5>
+                                <ul class="list-link">
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Đại hội cổ đông
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Điều lệ, quy chế
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Báo cáo tài chính
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Báo cáo thường niên
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Báo cáo quản trị
+                                        </a>
+                                    </li>
+                                    <li class="link-item">
+                                        <a href="" class="link">
+                                            Thông báo, thông tin khác
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-3 d-none d-xl-block">
+                    <div class="footer-link">
+                        <h5 class="footer-link-title">
+                            TUYỂN DỤNG
+                        </h5>
+                        <ul class="list-link">
+                            <li class="link-item">
+                                <a href="" class="link">
+                                    Tại sao chọn Vietcombank
+                                </a>
+                            </li>
+                            <li class="link-item">
+                                <a href="" class="link">
+                                    Thông tin tuyển dụng
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ft-bot">
+            <div class="container d-flex align-items-center">
+                <ul class="menu-link-ft">
+                    <li>
+                        <a href="#">
+                            Đăng ký/từ chối Email quảng cáo  
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Đăng ký nhận thông tin tỷ giá  
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Vietcombank Web Mail   
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Quản lý văn bản
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Điều khoản sử dụng  
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Sơ đồ Website  
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            © 2015 Vietcombank
+                        </a>
+                    </li>
+                </ul>
+                <ul class="social-link">
+                    <li>
+                        <h5>Hãy kết nối với chúng tôi</h5>
+                    </li>
+                    <li class="social-link">
+                        <a href="#">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#">
+                            <i class="fa fa-youtube-square" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </template>
