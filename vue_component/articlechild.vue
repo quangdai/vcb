c8205c4173e4063bff085ffc20929ce11ec0563d
vanquan94 <vanquan94bg@gmail.com>
2020-04-22 21:14:01 +0700

    update

--- vue_component/articlechild.vue
+++ vue_component/articlechild.vue
@@ -3,12 +3,12 @@
         <article class="article-post">
             <a href="tai-tro-von-luu-dong.html"><img src="../images/img-cat.jpg" alt="image"></a>
             <div class="title-post">
-                <a href="tai-tro-von-luu-dong.html">Tín dụng doanh nghiệp</a>
+                <a href="tai-tro-von-luu-dong.html">Quản lý tài sản</a>
             </div>
             <div class="excerpt">
                 Đây là hình thức cấp tín dụng kì hạn tối đa 12 tháng, trong đó Vietcombank đáp ứng nhu cầu...
             </div>
-            <a class="read-more" href="tai-tro-von-luu-dong.html"> Xem thêm >> </a>
+            <a class="read-more" href="tai-tro-von-luu-dong.html"> Chi tiết >> </a>
         </article>
     </div>
 </template>
