2e2a4c239425494929e14c8ecb59588098a28989
vanquan94 <vanquan94bg@gmail.com>
2020-04-25 22:04:28 +0700

    update

---
+++ vue_component/list-the.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="widget widget-category">
+        <div class="widget-title">THẺ</div>
+        <div class="widget-content multi-the">
+            <ul>
+                <li class="item-cat has-child open">
+                    <a href="#">Thẻ tín dụng quốc tế</a>
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    <ul class="sub-cat">
+                        <li class="item-cat-child current-item">
+                            <a href="#">So sánh các loại thẻ tín dụng quốc tế </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Cashplus Platinum American Express </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines Platinum American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines American Express® (Thẻ Bông Sen Vàng) </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Visa Platinum </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="item-cat has-child">
+                    <a href="#">Thẻ ghi nợ quốc tế</a>
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    <ul class="sub-cat">
+                        <li class="item-cat-child">
+                            <a href="#">So sánh các loại thẻ tín dụng quốc tế </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Cashplus Platinum American Express </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines Platinum American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines American Express® (Thẻ Bông Sen Vàng) </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Visa Platinum </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="item-cat has-child">
+                    <a href="#">Thẻ ghi nợ  nội địa</a>
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    <ul class="sub-cat">
+                        <li class="item-cat-child">
+                            <a href="#">So sánh các loại thẻ tín dụng quốc tế </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Cashplus Platinum American Express </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines Platinum American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Vietnam Airlines American Express® (Thẻ Bông Sen Vàng) </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank American Express® </a>
+                        </li>
+                        <li class="item-cat-child">
+                            <a href="#">Thẻ Vietcombank Visa Platinum </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="item-cat">
+                    <a href="#">Ưu đãi trả góp thẻ</a>
+                </li>
+            </ul>
+        </div>
+    </div>  
+</template>
---
+++ vue_component/product.vue
@@ -0,0 +1,60 @@
+<template>
+     <div class="product__info">
+        <img class="product__image" src="../images/VSignature.jpg" alt="Vietcombank American Express ® - Hạng vàng">
+        <h3 class="product__title li-0"><a href="">Vietcombank American Express ® - Hạng vàng</a></h3>
+        
+                <span class="product__year extra highlight li-1">50 triệu đến 500 triệu</span>
+        
+                <span class="product__year extra highlight li-2">​Lên đến 50 ngày</span>
+        
+                <span class="product__year extra highlight li-3">Không</span>
+        
+                <span class="product__year extra highlight li-4">Không</span>
+        
+                <span class="product__year extra highlight li-5">Có</span>
+        
+                <span class="product__year extra highlight li-6">Không</span>
+        
+                <span class="product__year extra highlight li-7">Không</span>
+        
+                <span class="product__year extra highlight li-8">Không</span>
+        
+                <span class="product__year extra highlight li-9">​Tối đa 105.000.000 VNĐ</span>
+        
+                <span class="product__year extra highlight li-10">Không</span>
+        
+                <span class="product__year extra highlight li-11">​Tích lũy và hoàn tiền 0.8% giá trị giao dịch​</span>
+        
+                <span class="product__year extra highlight li-12">Không</span>
+        
+                <span class="product__year extra highlight li-13">Không</span>
+        
+                <span class="product__year extra highlight li-14">Không</span>
+        
+                <span class="product__year extra highlight li-15">Có</span>
+        
+                <span class="product__year extra highlight li-16">Có</span>
+        
+                <span class="product__year extra highlight li-17">Có</span>
+        
+                <span class="product__year extra highlight li-18">Có</span>
+        
+                <span class="product__year extra highlight li-19">Không</span>
+        
+                <span class="product__year extra highlight li-20">Không</span>
+        
+                <span class="product__year extra highlight li-21">Có</span>
+        
+                <span class="product__year extra highlight li-22">Không</span>
+        
+                <span class="product__year extra highlight li-23">Có</span>
+        
+                <span class="product__year extra highlight li-24">Có</span>
+        
+                <span class="product__year extra highlight li-25">Có</span>
+        
+                <span class="product__year extra highlight li-26">Có</span>
+        
+        <span class="product__year extra highlight compare-more" ><a href="">Chi tiết</a></span>
+    </div>
+</template>
