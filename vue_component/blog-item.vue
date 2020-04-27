def140b52dbd289fec2198b1d156622144746f94
daidq <daidoquang@vccorp.vn>
2020-04-27 22:31:00 +0700

    update

--- vue_component/blog-item.vue
+++ vue_component/blog-item.vue
@@ -2,10 +2,8 @@
     <div class="col-lg-4">
         <div class="item-blog">
             <a href="tai-tro-von-luu-dong.html">
-                <img src="../images/img-blog.jpg" alt="Image Blog">
-                <ul>
-                    <li>Vietcombank đồng hành cùng chương trình "Xuân quê hương 2016"</li>
-                </ul>
+                <img src="images/img-blog.jpg" alt="Image Blog">
+                <h4 class="title">Vietcombank đồng hành cùng chương trình "Xuân quê hương 2016"</h4>
             </a>
         </div>
     </div>
