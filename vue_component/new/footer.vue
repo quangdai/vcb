46a43bf7e7d11efd97edae1111909c346efa2ba0
daidq <daidoquang@vccorp.vn>
2020-04-29 01:01:03 +0700

    update

---
+++ vue_component/new/footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer class="container">
+        <div class="menu-footer">
+            <ul class="menu">
+                <li class="menu-item">
+                    <a href="" class="menu-link">
+                        Liên hệ
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">
+                        Tuyển dụng
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">
+                        Về đầu trang
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="info">
+            <div class="left">
+                <p><strong>​Trang thông tin điện tử Ngân hàng TMCP Ngoại thương Việt Nam - Vietcombank</strong></p>
+                <p><strong>Chịu trách nhiệm nội dung:</strong> Tiến sỹ Phạm Mạnh Thắng - Phó Tổng giám đốc</p>
+                <p><strong>Liên hệ:</strong> Ban biên tập - Phòng Quan hệ Công chúng - Tòa nhà VCB Tower - 198 Trần Quang Khải, Hoàn Kiếm, Hà Nội.</p>
+            </div>
+            <div class="right">
+                <p><strong>Điện thoại: </strong>84-24-38243524</p>
+                <p><strong>Fax:</strong> 84-24-39365402</p>
+                <p><strong>Email:</strong> vcbnews.ho@vietcombank.com.vn​</p>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ vue_component/new/header.vue
@@ -0,0 +1,82 @@
+<template>
+    <header>
+        <div class="top-bar">
+            <div class="container top-bar-wrap">
+                <div class="left">
+                    <a href="tel:84 - 24 - 3824 8919" class="top-bar-phone">84 - 24 - 3824 8919</a>
+                </div>
+                <div class="right">
+                    <span class="top-bar-time">
+                        Thứ tư, 29/04/2020, 00:46 GMT+7 
+                    </span>
+                    <a href="#">
+                        <img src="images/ic_en.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header-main">
+            <div class="container">
+                <div class="banner-header">
+                    <a href="#">
+                        <img src="images/banner-new-home.png" alt="">
+                    </a>
+                </div>
+                <nav class="menu-wrap">
+                    <ul class="menu">
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                <img src="images/ic_home.png" alt="">
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Sự kiện nổi bật
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Tin tức
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Tin khuyến mãi
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Tin sản phẩm dịch vụ
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Thư viện ảnh - phim
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Phát mại tài sản
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Nghiên cứu - Trao đổi
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Nhịp cầu xanh
+                            </a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="menu-link">
+                                Tôi yêu Vietcombank
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
