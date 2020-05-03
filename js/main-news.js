fbbb524593c2aa160867da1a16abb5d83a31ee36
daidq <daidoquang@vccorp.vn>
2020-05-04 00:20:18 +0700

    update

--- js/main-news.js
+++ js/main-news.js
@@ -1,36 +1,40 @@
 // js slider home
 $(window).bind("load", function() {
     //  video slider
-    $('.right-news .slider-video').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots:false,
-        items:1
-    })
+    if($('.right-news .slider-video').length){
+        $('.right-news .slider-video').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:false,
+            items:1
+        })
+    }
 
     // TVA
-    $('.thu-vien-anh .slider-tva').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        margin:5,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:2,
-                nav:true
-            },
-            1000:{
-                items:3,
-                nav:true,
-                loop:true
+    if($('.thu-vien-anh .slider-tva').length){
+        $('.thu-vien-anh .slider-tva').owlCarousel({
+            loop:true,
+            margin:10,
+            responsiveClass:true,
+            margin:5,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:2,
+                    nav:true
+                },
+                1000:{
+                    items:3,
+                    nav:true,
+                    loop:true
+                }
             }
-        }
-    })
+        });
+    }
  });
 
  // Tab home
@@ -89,110 +93,112 @@ $(window).bind("load", function() {
 // slider thu vien anh
 
 $(window).bind("load", function() {
-    var bigimage = $("#thu-vien-anh");
-    var thumbs = $("#thumbs");
-    //var totalslides = 10;
-    var syncedSecondary = true;
-    
-    bigimage
-        .owlCarousel({
-        items: 1,
-        slideSpeed: 4000,
-        nav: true,
-        autoplay: false,
-        dots: false,
-        loop: false,
-        responsiveRefreshRate: 200,
-        navText: [
-        '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-        '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
-        ]
-    })
-        .on("changed.owl.carousel", syncPosition);
-    
-    thumbs
-        .on("initialized.owl.carousel", function() {
+    if($("#thu-vien-anh").length){
+        var bigimage = $("#thu-vien-anh");
+        var thumbs = $("#thumbs");
+        //var totalslides = 10;
+        var syncedSecondary = true;
+        
+        bigimage
+            .owlCarousel({
+            items: 1,
+            slideSpeed: 4000,
+            nav: true,
+            autoplay: false,
+            dots: false,
+            loop: false,
+            responsiveRefreshRate: 200,
+            navText: [
+            '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+            '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+            ]
+        })
+            .on("changed.owl.carousel", syncPosition);
+        
         thumbs
-        .find(".owl-item")
-        .eq(0)
-        .addClass("current");
-    })
-        .owlCarousel({
-        items: 5,
-        dots: false,
-        nav: true,
-        navText: [
-        '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-        '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
-        ],
-        smartSpeed: 200,
-        slideSpeed: 500,
-        slideBy: 1,
-        responsiveRefreshRate: 100,
-        responsive:{
-            320:{
-                items:3,
-            },
-            800:{
-                items:3,
-            },
-            1024:{
-                items:5,
+            .on("initialized.owl.carousel", function() {
+            thumbs
+            .find(".owl-item")
+            .eq(0)
+            .addClass("current");
+        })
+            .owlCarousel({
+            items: 5,
+            dots: false,
+            nav: true,
+            navText: [
+            '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+            '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+            ],
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: 1,
+            responsiveRefreshRate: 100,
+            responsive:{
+                320:{
+                    items:3,
+                },
+                800:{
+                    items:3,
+                },
+                1024:{
+                    items:5,
+                }
+            }
+        })
+            .on("changed.owl.carousel", syncPosition2);
+        
+        function syncPosition(el) {
+            //if loop is set to false, then you have to uncomment the next line
+            //var current = el.item.index;
+        
+            //to disable loop, comment this block
+            var count = el.item.count - 1;
+            var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+        
+            if (current < 0) {
+            current = count;
+            }
+            if (current > count) {
+            current = 0;
+            }
+            //to this
+            thumbs
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+            var onscreen = thumbs.find(".owl-item.active").length - 1;
+            var start = thumbs
+            .find(".owl-item.active")
+            .first()
+            .index();
+            var end = thumbs
+            .find(".owl-item.active")
+            .last()
+            .index();
+        
+            if (current > end) {
+            thumbs.data("owl.carousel").to(current, 100, true);
+            }
+            if (current < start) {
+            thumbs.data("owl.carousel").to(current - onscreen, 100, true);
             }
         }
-    })
-        .on("changed.owl.carousel", syncPosition2);
-    
-    function syncPosition(el) {
-        //if loop is set to false, then you have to uncomment the next line
-        //var current = el.item.index;
-    
-        //to disable loop, comment this block
-        var count = el.item.count - 1;
-        var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
-    
-        if (current < 0) {
-        current = count;
+        
+        function syncPosition2(el) {
+            if (syncedSecondary) {
+            var number = el.item.index;
+            bigimage.data("owl.carousel").to(number, 100, true);
+            }
         }
-        if (current > count) {
-        current = 0;
-        }
-        //to this
-        thumbs
-        .find(".owl-item")
-        .removeClass("current")
-        .eq(current)
-        .addClass("current");
-        var onscreen = thumbs.find(".owl-item.active").length - 1;
-        var start = thumbs
-        .find(".owl-item.active")
-        .first()
-        .index();
-        var end = thumbs
-        .find(".owl-item.active")
-        .last()
-        .index();
-    
-        if (current > end) {
-        thumbs.data("owl.carousel").to(current, 100, true);
-        }
-        if (current < start) {
-        thumbs.data("owl.carousel").to(current - onscreen, 100, true);
-        }
+        
+        thumbs.on("click", ".owl-item", function(e) {
+            e.preventDefault();
+            var number = $(this).index();
+            bigimage.data("owl.carousel").to(number, 300, true);
+        });
     }
-    
-    function syncPosition2(el) {
-        if (syncedSecondary) {
-        var number = el.item.index;
-        bigimage.data("owl.carousel").to(number, 100, true);
-        }
-    }
-    
-    thumbs.on("click", ".owl-item", function(e) {
-        e.preventDefault();
-        var number = $(this).index();
-        bigimage.data("owl.carousel").to(number, 300, true);
-    });
 });
  
 // Js lightgallery
